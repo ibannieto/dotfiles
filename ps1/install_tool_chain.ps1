dd54d24224b8514580f2c0c7895e778c2a672c55
Iban Nieto <ibannieto@users.noreply.github.com>
2019-10-23 01:17:58 +0200

    Add hugo to chocolatey

--- ps1/install_tool_chain.ps1
+++ ps1/install_tool_chain.ps1
@@ -1,6 +1,6 @@
 # install tool chain
 
-$Packages = '7zip', 'awscli', 'conemu', 'coretemp', 'cpu-z', 'cue', 'curl', 'dep', 'docker-compose', 'docker-desktop', 'faas-cli', 'git', 'git-credential-manager-for-windows', 'github-desktop', 'golang', 'gpu-z', 'graphviz', 'greenshot', 'habitat', 'inspec', 'keepass', 'keepass-plugin-keeagent', 'keepass-plugin-keeanywhere', 'kubernetes-cli', 'kubernetes-helm', 'love', 'make', 'neovim', 'nodejs', 'qbittorrent', 'skaffold', 'slack', 'terraform', 'terragrunt', 'tflint', 'tiled', 'vlc', 'vlc-skins', 'vscode', 'vscode-docker', 'vscode-gitignore', 'vscode-icons', 'vscode-markdownlint', 'vscode-settingssync', 'vscode-yaml', 'zip'
+$Packages = '7zip', 'awscli', 'conemu', 'coretemp', 'cpu-z', 'cue', 'curl', 'dep', 'docker-compose', 'docker-desktop', 'faas-cli', 'git', 'git-credential-manager-for-windows', 'github-desktop', 'golang', 'gpu-z', 'graphviz', 'greenshot', 'habitat', 'inspec', 'keepass', 'keepass-plugin-keeagent', 'keepass-plugin-keeanywhere', 'kubernetes-cli', 'kubernetes-helm', 'make', 'neovim', 'nodejs', 'qbittorrent', 'skaffold', 'slack', 'terraform', 'terragrunt', 'tflint', 'tiled', 'vlc', 'vlc-skins', 'vscode', 'vscode-docker', 'vscode-gitignore', 'vscode-icons', 'vscode-markdownlint', 'vscode-settingssync', 'vscode-yaml', 'zip', 'hugo'
 
 ForEach ($PackageName in $Packages)
 {
